f6d7d57029e74bec0bf5ddc22a10709309e087c2
littleqz <qizhihere@gmail.com>
2016-06-30 07:02:19 +0800

    Initial commit.

---
+++ build.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env sh
+
+set -xe
+
+cd "$SUBDIR"
+
+VERSION=$(basename $(pwd))
+IMAGE=${IMAGE_PREFIX}${IMAGE_NAME}:${TAG_PREFIX}${VERSION}
+
+if [ -f .version ]; then
+    for x in $(cat .version); do
+        TAGS="$TAGS ${TAG_PREFIX}${x}"
+    done
+fi
+
+docker build -t $IMAGE -f Dockerfile .
+echo $IMAGE >> "$PUSH_LIST"
+
+for x in $TAGS; do
+    alias=${IMAGE%%:*}:$x
+    docker tag $IMAGE $alias
+    echo "$alias" >> "$PUSH_LIST"
+done
